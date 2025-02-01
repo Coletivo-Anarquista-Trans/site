7bb52e650295aae0f31d378cbaf9fecb749920d9
sara <sarateixeirastar@gmail.com>
2025-02-01 16:41:47 -0300

    deleted a buncha build stuff

--- .next/server/app/api/dinosaurs/[dinosaur]/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/dinosaurs/[dinosaur]/route.js";
-const runtime = require("../../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/cbc35_next_dist_d0bd04._.js");
-runtime.loadChunk("server/chunks/[root of the server]__7a25ab._.js");
-runtime.loadChunk("server/chunks/_ce42d3._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/dinosaurs/[dinosaur]/route/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.deno/next@15.1.6/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/dinosaurs/[dinosaur]/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
