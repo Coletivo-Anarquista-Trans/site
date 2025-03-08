fcb3125a54fe31ce790fa77f5f5dff87ff893f0e
Mazrine <mazrine@protonmail.com>
2025-03-08 14:04:58 -0300

    feat: mvp terminal emulator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,75 +5,52 @@ import Header from "@/components/Header";
 import {CyberButton} from "@/components/CyberButton"
 import {CyberContainer} from "@/components/CyberContainer"
 import classnames from "classnames";
+import CyberTerminal from "@/components/CyberTerminal";
 
 export default function Home() {
     const {theme, toggleTheme} = useTheme();
-
     return (
-        <>
-            <CyberContainer
-                className={classnames(theme)}>
-                <Header/>
-                <CyberContainer className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                    <CyberButton
-                        onClick={toggleTheme}
-                        unevenBorders
-                    >
-                        Toggle Theme
-                    </CyberButton>
-                    <CyberButton
-                        onClick={toggleTheme}
-                        normalBorders
-                        textGlow
-                    >
-                        Toggle Theme
-                    </CyberButton>
-                    <CyberButton
-                        onClick={toggleTheme}
-                        unevenBorders
-                        glowingBorders
-                    >
-                        Toggle Theme
-                    </CyberButton>
-                    <CyberButton
-                        onClick={toggleTheme}
-                        normalBorders
-                        glowingBorders
-                    >
-                        Toggle Theme
-                    </CyberButton>
-                    <CyberButton
-                        onClick={toggleTheme}
-                        unevenBorders
-                        clearBorders
-                    >
-                        Toggle Theme
-                    </CyberButton>
-                    <CyberButton
-                        onClick={toggleTheme}
-                        normalBorders
-                        clearBorders
-                    >
-                        Toggle Theme
-                    </CyberButton>
-                    <CyberButton
-                        onClick={toggleTheme}
-                        unevenBorders
-                        clearBorders
-                        secondaryTextGlow
-                    >
-                        Toggle Theme
-                    </CyberButton>
-                    <CyberButton
-                        onClick={toggleTheme}
-                        normalBorders
-                        clearBorders
-                        secondaryTextGlow
-                    >
-                        Toggle Theme
-                    </CyberButton>
-                </CyberContainer>
-            </CyberContainer>
-        </>
+      <>
+        <CyberContainer className={classnames(theme)}>
+          <Header />
+          <CyberTerminal theme={theme} />
+          <CyberContainer className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+            <CyberButton onClick={toggleTheme} unevenBorders>
+              Toggle Theme
+            </CyberButton>
+            <CyberButton onClick={toggleTheme} normalBorders textGlow>
+              Toggle Theme
+            </CyberButton>
+            <CyberButton onClick={toggleTheme} unevenBorders glowingBorders>
+              Toggle Theme
+            </CyberButton>
+            <CyberButton onClick={toggleTheme} normalBorders glowingBorders>
+              Toggle Theme
+            </CyberButton>
+            <CyberButton onClick={toggleTheme} unevenBorders clearBorders>
+              Toggle Theme
+            </CyberButton>
+            <CyberButton onClick={toggleTheme} normalBorders clearBorders>
+              Toggle Theme
+            </CyberButton>
+            <CyberButton
+              onClick={toggleTheme}
+              unevenBorders
+              clearBorders
+              secondaryTextGlow
+            >
+              Toggle Theme
+            </CyberButton>
+            <CyberButton
+              onClick={toggleTheme}
+              normalBorders
+              clearBorders
+              secondaryTextGlow
+            >
+              Toggle Theme
+            </CyberButton>
+          </CyberContainer>
+        </CyberContainer>
+      </>
     );
 }
