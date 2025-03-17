87acbfa2bfe79d54b1a9db5185fe6b4f119ba169
iniarichan <cami-ini@hotmail.com>
2025-03-16 21:12:02 -0300

    Quem somos page

--- src/app/sobre-nos/page.tsx
+++ src/app/sobre-nos/page.tsx
@@ -13,22 +13,68 @@ export default function QuemSomosNos() {
                 <CyberContainer className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                     <div className="inline-block justify-normal whitespace-normal p-8 break-words w-auto h-auto min-w-[48rem] max-w-md">
                         <CyberContainer unevenBorders glowingBorders className="text-center text-4xl p-4 m-14 w-auto h-auto">
-                            Quem Somos
+                            <b>Quem Somos</b>
                         </CyberContainer>
                         <CyberContainer className={"text-center text-base p-2 m-4 w-auto h-auto"}>
                             {/* Add your content here */}
                             <p>
-                                Somos um coletivo dedicado a promover a liberdade e a autonomia dos corpos e mentes marginalizados. Nossa missão é construir um mundo onde todas as pessoas possam viver plenamente suas identidades e expressões, sem as restrições impostas pelo sistema capitalista e patriarcal.
+                                Somos um coletivo buscando garantir a liberdade e <b>autonomia dos corpos e mentes marginalizados</b>. 
                             </p>
                             <br />
                             <p>
-                                Acreditamos na descentralização do poder e na construção de comunidades autônomas e colaborativas. Trabalhamos para garantir o acesso a tecnologias de afirmação corporal, saúde, moradia e alimentação para todos, especialmente para aqueles que são mais marginalizados pela sociedade.
+                                Todas as pessoas devem <b>dominar a produção do próprio corpo</b>, sem as restrições impostas pelo cistema.
                             </p>
                             <br />
                             <p>
-                                Nossa luta é pela justiça social, pela igualdade e pela liberdade. Junte-se a nós nessa jornada para transformar o mundo e construir um futuro melhor para todos.
+                                Acreditamos na construção de comunidades autônomas e colaborativas, proporcionando <b>experiências sociais fora do cistema</b>. 
+                            </p>
+                            <br />
+                            <p>
+                                Acesso a tecnologias de afirmação corporal, saúde, moradia e alimentação para todos, especialmente para aqueles que são mais marginalizados pela sociedade é nosso objetivo.
+                            </p>
+                            <br />
+                            <p>
+                                Retome as tecnologias roubadas de nós, não deixe-os ter nossos amores e corpos.
                             </p>
                         </CyberContainer>
+                    </div>
+                    {/* Integrantes container */}
+                    <div className="inline-block justify-normal whitespace-normal p-2 break-words w-auto h-auto min-w-[48rem] max-w-md">
+                        <CyberContainer unevenBorders glowingBorders className="text-center text-4xl p-1 m-1 w-auto h-auto">
+                            <b>Integrantes</b>
+                        </CyberContainer> 
+                    </div>
+                    {/* Side-by-side containers */}
+                    <div className="flex flex-row justify-normal whitespace-normal p-2 break-words w-auto h-auto min-w-[12rem] max-w-md">
+                        {/* Mazrine container */}
+                        <CyberContainer unevenBorders glowingBorders className="text-center text-4xl p-1 m-1 w-64 h-16">
+                            <b>Mazrine</b>
+                        </CyberContainer>
+                        {/* Lynx container */}
+                        <CyberContainer unevenBorders glowingBorders className="text-center text-4xl p-1 m-1 w-64 h-16">
+                            <b>Lynx</b>
+                        </CyberContainer>
+                        {/* Ini container */}
+                        <CyberContainer unevenBorders glowingBorders className="text-center text-4xl p-1 m-1 w-64 h-16">
+                            <b>Iniari</b>
+                        </CyberContainer>
+                        {/* V container */}
+                        <CyberContainer unevenBorders glowingBorders className="text-center text-4xl p-1 m-1 w-64 h-16">
+                            <b>V</b>
+                        </CyberContainer>
+                         {/* (member) container */}
+                        <CyberContainer unevenBorders glowingBorders className="text-center text-4xl p-1 m-1 w-64 h-16">
+                            <b>???</b>
+                        </CyberContainer>
+                         {/* (member) container */}
+                         <CyberContainer unevenBorders glowingBorders className="text-center text-4xl p-1 m-1 w-64 h-16">
+                            <b>???</b>
+                        </CyberContainer>
+                         {/* (member) container */}
+                         <CyberContainer unevenBorders glowingBorders className="text-center text-4xl p-1 m-1 w-64 h-16">
+                            <b>???</b>
+                        </CyberContainer>
+
                     </div>
                 </CyberContainer>
             </CyberContainer>
