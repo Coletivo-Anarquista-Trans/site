3ce63cc7a6b97d73c53137c01ddba44534e92014
saralynx <sarateixeirastar@gmail.com>
2025-03-17 21:39:02 -0300

    added cybertooltip component, based off of react-tooltip.

---
+++ src/components/atoms/CyberTooltip/index.tsx
@@ -0,0 +1,25 @@
+import { Tooltip } from "react-tooltip";
+import { useTheme } from "@/context/ThemeContext";
+
+interface CyberTooltipProps {
+    word: string;
+    description: string;
+}
+
+export default function CyberTooltip({ word, description }: CyberTooltipProps) {
+    const { theme } = useTheme();
+
+    return (
+        <>
+            <span data-tooltip-id={word}
+              className={`text-accent3 underline cursor-help inline-block transition-transform duration-200 ease-in-out hover:scale-110 ${theme}`}
+            >
+                {word}
+            </span>
+
+            <Tooltip id={word} place="top" >
+                {description}
+            </Tooltip>
+        </>
+    );
+}
