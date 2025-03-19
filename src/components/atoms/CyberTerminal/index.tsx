b4fb311a56b334f3c5098001ff6b4fbbc174d163
Mazrine <mazrine@protonmail.com>
2025-03-19 00:44:11 -0300

    upgrade: increase prismatic intent of the cyberdrone by 44%. Realocated glyph decoder in order to have internal global range. fixed mishapen circuitry and added preparations for psychometric addons

--- src/components/atoms/CyberTerminal/index.tsx
+++ src/components/atoms/CyberTerminal/index.tsx
@@ -1,21 +1,20 @@
 import { useState, useRef, useEffect } from "react";
 import { kaomojis } from "@/utils/kaomojis";
 import dynamic from "next/dynamic"; // Import dynamic from Next.js
+import { useTheme } from "@/context/ThemeContext";
+import { useFont } from "@/context/FontContext"; // Import the useFont hook
 
 // Dynamically import IconTint with SSR disabled
 const IconTint = dynamic(() => import("react-icon-tint"), { ssr: false });
 
-interface CyberTerminalProps {
-}
+interface CyberTerminalProps {}
 
 export default function CyberTerminal({}: CyberTerminalProps) {
-  const [history, setHistory] = useState<string[]>([
-    "Welcome to Fake Terminal!",
-    "",
-    "<pre> _._     _,-'\"\"`-._\n(,-.`._,'(       |\\`-/|\n    `-.-' \\ )-`( , o o)\n          `-    \\`_`\"'-</pre>",
-  ]);
+  const [history, setHistory] = useState<string[]>(["Miauzers!", ""]);
   const [input, setInput] = useState("");
   const terminalRef = useRef<HTMLDivElement>(null);
+  const { theme } = useTheme();
+  const { font, fontSize } = useFont(); // Use the useFont hook
 
   const getRandomKaomoji = () => {
     const randomIndex = Math.floor(Math.random() * kaomojis.length);
@@ -55,7 +54,7 @@ export default function CyberTerminal({}: CyberTerminalProps) {
 
   // Define color mappings for each theme
   const themeColors: Record<string, string> = {
-    "dark-violet": "#6a0dad",
+    "dark-violet": "#301055FF",
     "neon-aqua": "#00ffff",
     "cyberpunk-violet": "#a020f0",
     "cyberpunk-crimson": "#dc143c",
@@ -63,30 +62,32 @@ export default function CyberTerminal({}: CyberTerminalProps) {
 
   return (
     <div
-      className={`p-4 rounded-lg w-full max-w-2xl mx-auto h-96 overflow-y-auto font-mono border bg-background text-foreground border-accent3 granular-effect`}
+      className={`p-4 rounded-lg w-full mx-auto h-96 overflow-y-auto font-mono border-4 bg-background text-accent-5 border-accent1 granular-effect rounded-tl-[20px] rounded-br-[20px] rounded-bl-[0px] rounded-tr-[0px]`}
+      style={{ fontFamily: font, fontSize: `${fontSize}px` }} // Apply font and font size
     >
-      {history.map((line, index) => (
-        <div
-          key={index}
-          className="mb-1"
-          dangerouslySetInnerHTML={{ __html: line }}
-        />
-      ))}
-      <div ref={terminalRef} />
-
-      {/* Render IconTint with dynamic color based on theme */}
       <IconTint
         maxHeight={150}
         maxWidth={150}
         color={themeColors[theme] || "#ffffff"}
         src="cats.png"
       />
+      {history.map((line, index) => (
+        <div
+          key={index}
+          // Apply text-accent5 for commands (lines starting with "$") and text-accent1 for responses
+          className={`mb-1 ${
+            line.startsWith("$") ? "text-accent5" : "text-accent1"
+          }`}
+          dangerouslySetInnerHTML={{ __html: line }}
+        />
+      ))}
+      <div ref={terminalRef} />
 
       <form onSubmit={handleSubmit} className="flex">
-        <span className="text-foreground">$</span>
+        <span className="text-accent5">$</span>
         <input
           type="text"
-          className="bg-transparent border-none outline-none ml-2 w-full text-foreground"
+          className="bg-transparent border-none outline-none ml-2 w-full text-accent5"
           value={input}
           onChange={(e) => setInput(e.target.value)}
           autoFocus
