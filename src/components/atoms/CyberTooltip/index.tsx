aa0f19195a46528cae720f84987664bf98f32c47
iniarichan <cami-ini@hotmail.com>
2025-03-23 22:05:13 -0300

    adicionei os negritos e alguns tooltipis + glossário no final da página

--- src/components/atoms/CyberTooltip/index.tsx
+++ src/components/atoms/CyberTooltip/index.tsx
@@ -3,7 +3,7 @@ import { useTheme } from "@/context/ThemeContext";
 
 interface CyberTooltipProps {
     word: string;
-    description: string;
+    description: string; 
 }
 
 export default function CyberTooltip({ word, description }: CyberTooltipProps) {
@@ -12,12 +12,12 @@ export default function CyberTooltip({ word, description }: CyberTooltipProps) {
     return (
         <>
             <span data-tooltip-id={word}
-              className={`text-accent3 underline cursor-help inline-block transition-transform duration-200 ease-in-out hover:scale-110 ${theme}`}
+              className={`text-accent3  underline cursor-help inline-block transition-transform duration-200 ease-in-out hover:scale-110 ${theme}`}
             >
                 {word}
             </span>
 
-            <Tooltip id={word} place="top" >
+            <Tooltip id={word} place="top" > 
                 {description}
             </Tooltip>
         </>
