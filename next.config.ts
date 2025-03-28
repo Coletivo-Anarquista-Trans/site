efa4e04127ef3ed31f2265118f018fb8641d051b
saralynx <sarateixeirastar@gmail.com>
2025-03-28 11:00:20 -0300

    removed deno.lock from .gitignore, edited workflow file to use deno instead of node

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: '/',
-  assetPrefix: '/',
+  basePath: isGithubPages ? "/" : "",
+  assetPrefix: isGithubPages ? "/" : "",
   eslint: {
     ignoreDuringBuilds: true,
   },
