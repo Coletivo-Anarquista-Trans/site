4e0a6143875d003e4430d7db5f3eee2e71604d62
iniari <cami-ini@hotmail.com>
2025-04-21 20:01:56 -0300

    Revert "Ajustes no texto do blog"

--- src/assets/blogPosts/post.ts
+++ src/assets/blogPosts/post.ts
@@ -19,16 +19,16 @@ export const posts: BlogPost[] = [
     image: "marcha.jpeg",
     content: `
       <h2>marcha  transmasc 30-03-2025</h2>
-      <p><br/>ao perder o costume de escrever me vejo como criança: começo, apago, vejo e revejo, me repreendo, me anulo. engraçado, fui assim quando criança.</p>
-<p><br/>ainda sou, me vigio passo-a-passo, sou meu próprio cão e adestrador, mantenho as ovelhas dentro do cercado e essa cerca se estreita, sufoca.</p>
-<p><br/>nesse dia que me vejo ao lado de tantos iguais a mim sinto o lapso de alívio desse sufoco, de tanto tentar acertar e me pegar errando. </p>
-<p><br/>talvez seja a maturidade dos quase três anos vivendo a transgeneridade, ou ter transcentrado as minhas relações, o apoio dos meus pais e não receber mais os olhares estranhos depois de alguns anos em terapia hormonal, talvez tenha sido ler joão walter nery e paul preciado. ou tudo junto, somado ao fato de que eu estava rodeado pelos meus e ali, gritando em uníssono por nossas vidas, pela primeira vez em muito tempo não senti medo de ser eu, e de estar errado em ser. </p>
-<p><br/>ainda que sejamos afrontados com a realidade das ruas e suas contradições, as violências que nos atravessam já ardem há tanto tempo que vezenquando passam batido, esquecemos, deixa passar. </p>
-<p><br/>veja bem, me adestrei por anos pra ser o que queriam de mim, soube ser comportada, silenciosa, soube não dar trabalho pros meus pais e pra minha família, fui um exemplo promissor até romper com uma lógica que, pra mim, nunca fez sentido. e ao escolher romper com isso também rompi com relações que me ofereceram como as essenciais pra que eu pudesse existir. desisti de muita coisa, sabia que a partir desse momento eu precisava garantir subsistência, existir pra construir o corpo que me fizesse bem, e isso não me faz nada além de dissidente. </p>
-<p><br/>quando converso com minhas amigas travestis que vivem a transgeneridade por muito mais tempo que eu, vejo a força de se manter vivo sendo açoitado todos os dias, sendo limitado por palavras, olhares, violências que gritam mas que também, por vezes, são tão silenciosas que passam batido aos nossos olhos, mas que marcam nossos corpos com ferro quente, faz sangrar igual.</p>
-<p><br/>se hoje, diferente de muitos como eu, me mantenho vivo e luto por isso é porque acredito que existir como um corpo transmasculino nesse mundo é revolucionário, não me acho importante mas em mim também não existe mais o cinismo de achar que diferentes formas de existir no mundo não são possíveis. utopia é liberdade, e é o que eu busco todos os dias, não só pra mim, não só pra pessoas trans, mas por todos que ja sentiram a dor da fome, a humilhação da miséria, do abuso, do preconceito, de se sentir indesejado, nojento, excluído, de sentir a frustração de não ter forças e de se ver sem saída, por todos que ja sentiram que a única opção era deixar de existir. </p>
-<p><br/>nossa luta é por trabalho, moradia, saúde, educação e dignidade, o básico para uma subsistência. mas também lutamos pelos afetos, por acessos, por outras maneiras de existir num mundo que nos aprisiona e que todos os dias reforça que não merecemos nem mesmo ser chamados pelos nomes que preferimos ser chamados. </p>
-<p><br/>a minha vida é luta, e vai ser até o final! laroyê! </p>
+      <p>ao perder o costume de escrever me vejo como criança: começo, apago, vejo e revejo, me repreendo, me anulo. engraçado, fui assim quando criança.
+ainda sou, me vigio passo-a-passo, sou meu próprio cão e adestrador, mantenho as ovelhas dentro do cercado e essa cerca se estreita, sufoca.
+nesse dia que me vejo ao lado de tantos iguais a mim sinto o lapso de alívio desse sufoco, de tanto tentar acertar e me pegar errando.
+talvez seja a maturidade dos quase três anos vivendo a transgeneridade, ou ter transcentrado as minhas relações, o apoio dos meus pais e não receber mais os olhares estranhos depois de alguns anos em terapia hormonal, talvez tenha sido ler joão walter nery e paul preciado. ou tudo junto, somado ao fato de que eu estava rodeado pelos meus e ali, gritando em uníssono por nossas vidas, pela primeira vez em muito tempo não senti medo de ser eu, e de estar errado em ser.
+ainda que sejamos afrontados com a realidade das ruas e suas contradições, as violências que nos atravessam já ardem há tanto tempo que vezenquando passam batido, esquecemos, deixa passar.
+veja bem, me adestrei por anos pra ser o que queriam de mim, soube ser comportada, silenciosa, soube não dar trabalho pros meus pais e pra minha família, fui um exemplo promissor até romper com uma lógica que, pra mim, nunca fez sentido. e ao escolher romper com isso também rompi com relações que me ofereceram como as essenciais pra que eu pudesse existir. desisti de muita coisa, sabia que a partir desse momento eu precisava garantir subsistência, existir pra construir o corpo que me fizesse bem, e isso não me faz nada além de dissidente.
+quando converso com minhas amigas travestis que vivem a transgeneridade por muito mais tempo que eu, vejo a força de se manter vivo sendo açoitado todos os dias, sendo limitado por palavras, olhares, violências que gritam mas que também, por vezes, são tão silenciosas que passam batido aos nossos olhos, mas que marcam nossos corpos com ferro quente, faz sangrar igual.
+se hoje, diferente de muitos como eu, me mantenho vivo e luto por isso é porque acredito que existir como um corpo transmasculino nesse mundo é revolucionário, não me acho importante mas em mim também não existe mais o cinismo de achar que diferentes formas de existir no mundo não são possíveis. utopia é liberdade, e é o que eu busco todos os dias, não só pra mim, não só pra pessoas trans, mas por todos que ja sentiram a dor da fome, a humilhação da miséria, do abuso, do preconceito, de se sentir indesejado, nojento, excluído, de sentir a frustração de não ter forças e de se ver sem saída, por todos que ja sentiram que a única opção era deixar de existir.
+nossa luta é por trabalho, moradia, saúde, educação e dignidade, o básico para uma subsistência. mas também lutamos pelos afetos, por acessos, por outras maneiras de existir num mundo que nos aprisiona e que todos os dias reforça que não merecemos nem mesmo ser chamados pelos nomes que preferimos ser chamados.
+a minha vida é luta, e vai ser até o final! laroyê! </p>
     `,
   },
   {
@@ -48,32 +48,27 @@ export const posts: BlogPost[] = [
     title: "foda-se o CFM",
     description: "ini",
     slug: "foda-se-o-cfm",
-    date: "2025-04-15",
+    date: "2025-05-15",
     image: "bastard-doctors.png",
     content: `
-      <h2><b>Sobre o CFM</b></h2>
-      <p>Mais uma vez o Conselho Federal de Medicina decidiu “legislar”, através de suas normativas, sobre os corpos trans com a desculpa de estar fazendo “saúde”, mesmo que nenhuma pessoa trans participe dessas decisões.</p>
-<p>Inspirada pelo CFM, decidi fundar a nossa contra-instituição para também normatizar sobre a cisgeneridade por estar preocupada com a saúde e o bem estar deles. </p>
-<br/>
-<p>O CIS-selho Foderal de Merdicina é o novo CFM responsável por essas diretrizes, das quais acreditamos que protegerá pessoas cis de se arrependerem posteriormente do constrangimento de suas decisões. Segue a normativa:</p>
-<br/>
-<p><b>I - É proibido chá de revelação de gênero de bebê.</p></b>
-<p>Justificativa: episódio de mania de adultos acreditando poder impor suas crenças de como uma pessoa que está a nascer deve se comportar pelo resto da sua vida. Por considerarmos essa crença absurda e trazer posteriormente sofrimento à criança que não pode se defender dos adultos, proibimos tal ritual.</p>
-<br/>
-<p><b>II- Proibição de prodedimentos estéticos e hormonização de pessoas cis para afirmar o gênero designado ao nascer.</p></b>
-<p>Justificativa: pessoas cis são seres naturais e sua beleza consiste nisso. O uso de procedimentos estéticos cirurgicos ou farmacológicos para afirmar o próprio corpo contraria a natureza humana, criada por deus, então para evitar sua auto-destruição, a cisgenereidade não pode de forma alguma alterar o próprio corpo pois isso pode levar a uma perda da própria identidade cis e arrependimento. Acreditamos ser essa a forma mais saudável para pessoas cis, afinal o contrário disso seria aceitar que gênero é construído por técnicas e tecnologias, mas sabemos que a cisgenereidade é natural - é o que eles dizem.</p>
-<br/>
-<p><b>III- Proibido o uso de drogas para aumento de performance sexual (tadalla e viagra para homem cis brocha). </p></b>
-<p>Justificativa: essa é exclusivamente porque acreditamos que os médicos do CFM usam e isso está claramente afetando a capacidade cognitiva deles de tomarem decisões ao ponto de achar que sabem algo sobre o melhor para pessoas trans.</p>
-<br/>
-<p>Ass: Presidenta do CFM, Penélope Lutan</p>
-<br></br>
-<p>É claro que a ironia do novo CFM, o Cis-selho como contra-instituição, só pode ser uma ironia porque nós não dominamos os meios de produção dos corpos, ao contrário do verdadeiro CFM que afirma seu poder sobre o corpo e a vida, seu biopoder, através do domínio sobre essas técnicas e tecnologias enquanto nos exclui delas.</p>
-<br/><p>O que está demonstrado é a classe médica se posicionando na ala mais conservadora e anti-trans junto da extrema-direita brasileira, apontando para a necessidade de também estarmos conectados para evitar mais restrições sobre nossos corpos e prontos para responder quando necessário.</p>
-<br/><p>Esse embate deve aumentar mais com a apresentação do Programa de Atenção à Saúde da População Trans (PAES Pop Trans), que busca construir um apoio à saúde trans mais focada em nos garantir os acessos a hormônios e apoio médico para nos dar maior autonomia sobre nosso corpo.</p>
-<br/><p>Lutamos para que no fim todos tenham acesso à construção do próprio corpo, no curto prazo isso significa garantir que programas como esse, que possam atender essas necessidades, sejam garantidos e não derrubados por “extra-legislações” e normativas produzidas entre a classe médica e política de direita. </p>
-<br/><p>Façamos o apoio aos poucos programas que ainda temos para ajudar a  população trans a ter acesso às suas necessidades de afirmação corporal e, no longo prazo, continuemos a construir nossas próprias formas de organização e construção corporal e comunitária para não continuar a depender das instituições cisgêneras.</p>
-<br/><p>Será ainda necessário produzir contra-instituições dos corpos dissidentes, capazes de produzirem os recursos, linguagens e agenciamentos capazes de atender as necessidades da nossa comunidade. <b>A cisgenereidade não fará o papel do nos garantir nossos corpos.</b></p>
+      <h2>Sobre o CFM</h2>
+      <p>Mais uma vez o Conselho Federal de Medicina decidiu “legislar”, através de suas normativas, sobre os corpos trans com a desculpa de estar fazendo “saúde”, mesmo que nenhuma pessoa trans participe dessas decisões.
+Inspirada pelo CFM, decidi fundar a nossa contra-instituição para também normatizar sobre a cisgeneridade por estar preocupada com a saúde e o bem estar deles. 
+O CIS-selho Foderal de Merdicina é o novo CFM responsável por essas diretrizes, das quais acreditamos que protegerá pessoas cis de se arrependerem posteriormente do constrangimento de suas decisões. Segue a normativa:
+I - É proibido chá de revelação de gênero de bebê.
+Justificativa: episódio de mania de adultos acreditando poder impor suas crenças de como uma pessoa que está a nascer deve se comportar pelo resto da sua vida. Por considerarmos essa crença absurda e trazer posteriormente sofrimento à criança que não pode se defender dos adultos, proibimos tal ritual.
+II- Proibição de prodedimentos estéticos e hormonização de pessoas cis para afirmar o gênero designado ao nascer.
+Justificativa: pessoas cis são seres naturais e sua beleza consiste nisso. O uso de procedimentos estéticos cirurgicos ou farmacológicos para afirmar o próprio corpo contraria a natureza humana, criada por deus, então para evitar sua auto-destruição, a cisgenereidade não pode de forma alguma alterar o próprio corpo pois isso pode levar a uma perda da própria identidade cis e arrependimento. Acreditamos ser essa a forma mais saudável para pessoas cis, afinal o contrário disso seria aceitar que gênero é construído por técnicas e tecnologias, mas sabemos que a cisgenereidade é natural - é o que eles dizem.
+III- Proibido o uso de drogas para aumento de performance sexual (tadalla e viagra para homem cis brocha). 
+Justificativa: essa é exclusivamente porque acreditamos que os médicos do CFM usam e isso está claramente afetando a capacidade cognitiva deles de tomarem decisões ao ponto de achar que sabem algo sobre o melhor para pessoas trans.
+Ass: Presidenta do CFM, Penélope Lutan
+----
+É claro que a ironia do novo CFM, o Cis-selho como contra-instituição, só pode ser uma ironia porque nós não dominamos os meios de produção dos corpos, ao contrário do verdadeiro CFM que afirma seu poder sobre o corpo e a vida, seu biopoder, através do domínio sobre essas técnicas e tecnologias enquanto nos exclui delas.
+O que está demonstrado é a classe médica se posicionando na ala mais conservadora e anti-trans junto da extrema-direita brasileira, apontando para a necessidade de também estarmos conectados para evitar mais restrições sobre nossos corpos.
+Esse embate deve aumentar mais com a apresentação do Programa de Atenção à Saúde da População Trans (PAES Pop Trans), que busca construir um apoio à saúde trans mais focada em nos garantir os acessos a hormônios e apoio médico para nos dar maior autonomia sobre nosso corpo.
+Lutamos para que no fim todos tenham acesso à construção do próprio corpo, no curto prazo isso significa garantir que programas como esse, que possam atender essas necessidades, sejam garantidos e não derrubados por “extra-legislações” e normativas produzidas entre a classe médica e política de direita. 
+Façamos o apoio aos poucos programas que ainda temos para ajudar a  população trans a ter acesso às suas necessidades de afirmação corporal e, no longo prazo, continuemos a construir nossas próprias formas de organização e construção corporal e comunitária para não continuar a depender das instituições cisgêneras.
+</p>
     `,
   },
   // Add more posts here
