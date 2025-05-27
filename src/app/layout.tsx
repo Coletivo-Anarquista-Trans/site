d3061415d519f913368ac3cc8ebbb9bc398420eb
neolemv <nanowebcontato@gmail.com>
2025-05-27 00:17:02 -0300

    fix: eu só queria que o favicon funcionasse

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   description: "Coletivo Anarquista Trans",
   icons: {
     icon: [
-      { url: "/cats.png", type: "image/png" },
+      { url: "/favicon.ico", type: "image/x-icon" },
       { url: "/cat-icon.svg", type: "image/svg+xml" },
       { url: "/cats.png", type: "image/png", sizes: "512x512" }
     ],
@@ -40,10 +40,12 @@ export default function RootLayout({
 }) {
   const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
   const imagePreloadHref = isGitHubPages ? "/site/cats.png" : "/cats.png";
-  
+  const faviconHref = isGitHubPages ? "/site/favicon.ico" : "/favicon.ico";
+
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href={faviconHref} />
         <link rel="preload" href={imagePreloadHref} as="image" />
       </head>
       <body>
