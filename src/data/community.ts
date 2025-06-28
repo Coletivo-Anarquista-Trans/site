a0305352a579b0b09b3ce7c0c8812422b2cd2d46
neolemv <nanowebcontato@gmail.com>
2025-06-28 17:40:28 -0300

    feat: adicionando descriçao para coletivo anarcokuir

--- src/data/community.ts
+++ src/data/community.ts
@@ -4,35 +4,35 @@ import { CyberCommunity } from "@/components/molecules/CyberCommunityCard";
 export const communityByCategory: Record<string, CyberCommunity[]> = {
     Coletivos: [
         {
-            id: '6',
+            id: '1',
             title: 'Acervo trans-anarquista',
             type: 'link',
             url: 'https://transanarquismo.noblogs.org/acervo/',
             description: 'Acervo com textos, livros e artigos sobre dissidências, perspectivas trans-anarquistas sobre o regime de gênero e como resistir e abolí-lo pela libertação do corpo.'
         },
         {
-            id: '7',
+            id: '2',
             title: 'Coletivo Transcentrado',
             type: 'link',
             url: 'https://coletivotranscentrado.com.br/',
             description: 'Site do coletivo, onde você pode encontrar informações sobre processo de transição, e um discord amigável para pessoas trans e com deficiência.'
         },
         {
-            id: '8',
+            id: '3',
             title: 'Villain Era',
             type: 'link',
             url: "https://villain-era.carrd.co/#guerra",
             description: 'Card com informações sobre o coletivo, formado por artistas trans de Juiz de Fora (MG), que tem o objetivo de expor, enaltecer, compartilhar a arte e a poética de seus alunes Trans...e dominar o mundo! Todes criadores e suas diversidades de gênero são bem vindes.'
         },
         {
-            id: '9',
+            id: '4',
             title: 'Revolta Anarkokuir',
             type: 'link',
             url: 'https://anarkokuir.hotglue.me/',
-            description: 'Coletivo anarcopunk queer.'
+            description: 'Grupo anarco-kuir que busca confrontar a assimilação dos movimentos LGBTs e elaborar uma anti-política queer e anarquista contra o Estado, o capital e todas as formas de opressão.'
         },
         {
-            id: '10',
+            id: '5',
             title: 'Revista Estudos Transviades',
             type: 'link',
             url: 'https://www.revistaestudostransviades.com/',
